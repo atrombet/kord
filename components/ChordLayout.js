f56576edd4999dd4c886bec20c1ab3c8b836a918
Anthony Trombetta <atrombet@gmail.com>
2020-02-04 08:07:11 -0600

    Optimize Playback of Sounds (#4)
    Changelist:
    * Move creation of sounds up to chord layout.
    * Use state to determine if a sound is being played when another is triggered.

--- components/ChordLayout.js
+++ components/ChordLayout.js
@@ -1,40 +1,59 @@
-import React from 'react';
-import {CHORD_MAP} from '../constants';
+import React, {useState} from 'react';
+import {CHORD_MAP, NUMBER_MAP} from '../constants';
 import {Col, Row, Grid} from 'react-native-easy-grid';
 import ChordButton from './ChordButton';
+import Sound from 'react-native-sound';
 
 const ChordLayout = ({selectedKey, numbers}) => {
+  const [playing, setPlaying] = useState(null);
+
   const chords = Object.keys(selectedKey.chords).reduce(
     (chordMap, chordNum) => {
+      const chordInfo = CHORD_MAP.get(selectedKey.chords[chordNum]);
       return {
         ...chordMap,
-        [chordNum]: CHORD_MAP.get(selectedKey.chords[chordNum]),
+        [chordNum]: {
+          ...chordInfo,
+          sound: new Sound(chordInfo.fileName, Sound.MAIN_BUNDLE),
+          number: NUMBER_MAP[chordNum],
+        },
       };
     },
     {},
   );
+
+  const playChord = chordId => {
+    setPlaying(currentlyPlaying => {
+      if (currentlyPlaying) {
+        currentlyPlaying.stop();
+      }
+      chords[chordId].sound.play();
+      return chords[chordId].sound;
+    });
+  };
+
   return (
     <Grid>
       <Col></Col>
       <Col>
         <Row></Row>
-        <Row><ChordButton numbers={numbers} number="5/7" chord={chords.five_seven}/></Row>
+        <Row><ChordButton numbers={numbers} chord={chords.five_seven} playSound={() => playChord('five_seven')}/></Row>
         <Row></Row>
-        <Row><ChordButton numbers={numbers} number="6-" chord={chords.six}/></Row>
+        <Row><ChordButton numbers={numbers} chord={chords.six} playSound={() => playChord('six')}/></Row>
         <Row></Row>
       </Col>
       <Col>
-        <Row><ChordButton numbers={numbers} number="2-" chord={chords.two}/></Row>
+        <Row><ChordButton numbers={numbers} chord={chords.two} playSound={() => playChord('two')}/></Row>
         <Row></Row>
-        <Row><ChordButton numbers={numbers} number="1" chord={chords.one}/></Row>
+        <Row><ChordButton numbers={numbers} chord={chords.one} playSound={() => playChord('one')}/></Row>
         <Row></Row>
-        <Row><ChordButton numbers={numbers} number="5" chord={chords.five}/></Row>
+        <Row><ChordButton numbers={numbers} chord={chords.five} playSound={() => playChord('five')}/></Row>
       </Col>
       <Col>
         <Row></Row>
-        <Row><ChordButton numbers={numbers} number="1/3" chord={chords.one_three}/></Row>
+        <Row><ChordButton numbers={numbers} chord={chords.one_three} playSound={() => playChord('one_three')}/></Row>
         <Row></Row>
-        <Row><ChordButton numbers={numbers} number="4" chord={chords.four}/></Row>
+        <Row><ChordButton numbers={numbers} chord={chords.four} playSound={() => playChord('four')}/></Row>
         <Row></Row>
       </Col>
       <Col></Col>
