cdb17e00bc042b71631da9c49b78bf0895125592
Anthony Trombetta <atrombet@gmail.com>
2020-04-07 20:12:38 -0500

    Update Labels For Switch
    Changelist:
    * Make labels Name and Numbers.
    * Update styles to layout in row.
    * Remove unused styles.

--- components/NumberNameSwitch.js
+++ components/NumberNameSwitch.js
@@ -5,8 +5,9 @@ import styles from '../styles/styles';
 const NumberNameSwitch = ({numbers, onValueChange}) => {
   return (
     <View style={styles.numberNameSwitch}>
+      <Text style={{...styles.numberNameSwitchText, textAlign: 'right'}}>Names</Text>
       <Switch value={numbers} onValueChange={onValueChange} />
-      <Text style={styles.numberNameSwitchText}>{numbers ? 'Numbers' : 'Names'}</Text>
+      <Text style={styles.numberNameSwitchText}>Numbers</Text>
     </View>
   );
 };
