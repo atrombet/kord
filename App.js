7eb410160bf786eb4fc5768f539af7f9cc11ce59
Anthony Trombetta <atrombet@gmail.com>
2020-04-08 22:24:59 -0500

    Remove unneeded import.

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StatusBar, Text} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 import styles from './styles/styles';
 import {
   ChordLayout,
